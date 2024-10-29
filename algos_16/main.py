9e239fa47df94bd49ae6295441468fbf5a446435
PC <abusyginza@gmail.com>
2024-10-29 07:45:37 +0700

    algos 29/10/2024 6 Kyu

--- algos_16/main.py
+++ algos_16/main.py
@@ -3,16 +3,8 @@ source_array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
 
 def sort_array(source_array):
-    odd = sorted([i for i in source_array if i % 2 != 0])
-    res = []
-    for i in source_array:
-        if i % 2 == 0:
-            res.append(i)
-        else:
-            res.append(odd[0])
-            odd.pop(0)
-    return res
-
+    odd = sorted([i for i in source_array if i % 2 != 0], reverse=True)
+    return [i if i % 2 == 0 else odd.pop() for i in source_array]
 
 
 print(sort_array(source_array))
