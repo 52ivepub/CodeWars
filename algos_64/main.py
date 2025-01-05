f09a061cb7f572b93a8d732f0eddb5b65dba3c80
Yugenyi Busygin <abusyginza@gmail.com>
2025-01-05 07:58:27 +0700

    05/01/2025 6 kyu

--- algos_64/main.py
+++ algos_64/main.py
@@ -1,29 +1,26 @@
-url = 'https://www.codewars.com/kata/5d23d89906f92a00267bb83d/train/python'
+url = 'https://www.codewars.com/kata/59f08f89a5e129c543000069/train/python'
 
-order = "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
-# ),
-x =  "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+arry = ["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]
+# ),['codewars','picaniny','hubububo']
 
 
-def get_order(order):
-    menu = ["Burger", "Fries", "Chicken", 'Pizza', "Sandwich", "Onionrings", "Milkshake", "Coke"]
-    new_order = []
+def dup(arry):
+    res = []
     seed = ''
-    for i in order:
-        if seed.capitalize() in menu:
-            new_order.append(seed)
-            seed = ''
-        seed += i
-    new_order.append(seed)
-    res_dict = {x:0 for x in menu}
-    for i in new_order:
-        res_dict[i.capitalize()] += 1
-    result = ''
-    for x, y in res_dict.items():
-        result += (x + ' ') * y
-    return result.strip()
+    for word in arry:
+        for char in range(len(word)):
+            if len(seed) == 0:
+                seed += word[char]
+            if word[char] == seed[-1]:
+                continue
+            seed += word[char]
+        res.append(seed)
+        seed = ''
+    return res
 
 
-print(get_order(order))
 
-# print(sorted(x.split()))
+
+
+
+print(dup(arry))
